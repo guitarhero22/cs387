a65067f6abacc87f4339437331a21b217411de12
guitarhero22 <rushabhkanadia@gmail.com>
2021-05-05 10:39:11 +0530

    adding data generator

---
+++ data/testgen.py
@@ -0,0 +1,43 @@
+import sys
+from random import randint
+from secrets import token_hex
+
+if len(sys.argv) != 3:
+	print("python3 testgen.py <number of write batches> <number of ops in a batch>")
+	sys.exit(1)
+
+#sys.argv[1] is the number of batches, sys.argv[2] is the number of operations in each
+
+schema = open("schema.sql", "w")
+
+schema.write('''
+drop table if exists testmultiple;
+create table testmultiple(
+	keycol varchar(64) primary key,
+	valcol int
+);
+''')
+
+schema.close()
+
+batches = int(sys.argv[1])
+batchsize = int(sys.argv[2])
+
+for batch in range(batches):
+	#this generates writes, and the reads expected to return non-null stuff
+	#higher batch numbers are more recently touched stuff
+	writefile = open("writebatch"+str(batch)+".sql", "w")
+	read1file = open("read1batch"+str(batch)+".sql", "w")
+	read2file = open("read2batch"+str(batch)+".sql", "w")
+	for op in range(batchsize):
+		k = token_hex(32)
+		v = randint(0, 100)
+		writefile.write("insert into testmultiple values("+str(k)+", "+str(v)+") on conflict(keycol) do update valcol = "+str(v)+";\n")
+		if op%4 == 0:
+			read2file.write("select * from testmultiple where keycol ="+str(k)+";\n")
+		if op%10 == 0:
+			k1 = token_hex(32)
+			read1file.write("select * from testmultiple where keycol ="+str(k1)+";\n")
+	writefile.close()
+	read1file.close()
+	read2file.close()
