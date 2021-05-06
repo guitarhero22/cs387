29404f40c9063ad85b1d286551463c16239ae1c9
MIHIR VAHANWALA <mjv1528@gmail.com>
2021-05-06 22:41:34 +0530

    master sketch added

---
+++ scratch/include/master.hpp
@@ -0,0 +1,36 @@
+#include "RBTree/rbtree.hpp"
+#include "Bloom/bloom.hpp"
+#include "Core/Merge.hpp"
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+struct K
+{
+	char c[64];
+};
+
+struct V
+{
+	char c[64];
+};
+
+class Master{
+	public:
+	BinaryTree <K, V> *bintree, *reserve;
+	BloomFile <K> *bloom;
+	mutex memlock, fslock;
+	int recent, current;
+	
+	pair<K, V> dbread(K k);
+	void dbwrite(K k, V v);
+
+	void adjust();
+
+	void serve(string batchfile);
+
+	void run();
+
+	Master();
+};
