8492b49073d6a9390cfe3be1ea49656898d9d8f7
MIHIR VAHANWALA <mjv1528@gmail.com>
2021-05-07 03:42:32 +0530

    master constructor

--- scratch/src/master.cpp
+++ scratch/src/master.cpp
@@ -140,3 +140,52 @@ void Master::adjust(){
 	this->fslock.unlock();
 
 }
+
+Master::Master(){
+
+	//assumes that file<i> begins with a timestamp
+
+	//what files are we accessing?
+	string fs = "file";
+	for (int i=0; i<NUMFILES; i++){
+		filesys[i] = fs+to_string(i);
+	}
+	
+	string back = "backup";
+	for (int i=0; i<2; i++){
+		backups[i] = back+to_string(i);
+	}
+
+	bloomdump = "bloomdump";
+
+	tempfile = "tempfile";
+
+	//what is recent?
+
+	FILE *zeroth = fopen(filesys[0].c_str, "rb");
+	fseek(zeroth, 0, SEEK_SET);
+	char time_str0[128];
+	fread(time_str0, 1, 128, zeroth);
+	fclose(zeroth);
+	tm ts_max;
+	int candidate = 0;
+	strptime(time_str0, TIME_FMT, &ts_max);
+
+	for (int i=1; i<NUMFILES; i++){
+		FILE *ith = fopen(filesys[i].c_str, "rb");
+		fseek(ith, 0, SEEK_SET);
+		char time_stri[128];
+		fread(time_stri, 1, 128, ith);
+		fclose(ith);
+		tm ts_i;
+		strptime(time_stri, TIME_FMT, &ts_i);
+
+		if (difftime(mktime(&ts_max), mktime(&ts_i)) < 0)
+		{
+			candidate = i;
+			memcpy(&ts_max, &ts_i, sizeof(tm));
+		}
+	}
+
+	recent = candidate;
+}
